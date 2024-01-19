6f1951cb3047d15056c9cb7a2f38f10b2d3337bd
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2024-01-19 11:49:43 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        response = make_response(jsonify([message.to_dict() for message in messages]))
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username'],
+        )
+        db.session.add(message)
+        db.session.commit()
+        response = make_response(
+            jsonify(message.to_dict()), 201
+        )
+       
+    return response
 
-@app.route('/messages/<int:id>')
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
 
+    if request.method == 'PATCH':
+        newMessageData = request.get_json()
+        for key in newMessageData:
+            setattr(message, key, newMessageData[key])
+
+        db.session.add(message)
+        db.session.commit()
+        response = make_response(
+            jsonify(message.to_dict()))
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response = jsonify({'message': f'deleted message {id}'}) 
+    return response
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000)
--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
-
+from datetime import datetime
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -12,3 +12,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    username = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
